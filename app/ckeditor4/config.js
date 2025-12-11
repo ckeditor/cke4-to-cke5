5bf839a0338f56605eac7f213133d025e56bd5a0
Piotrek Koszuliński <pkoszulinski@gmail.com>
2025-12-11 13:35:21 +0100

    Dependencies and initial CKEditor 4 setup.

---
+++ app/ckeditor4/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.removePlugins = 'image2';
+	config.extraPlugins = 'image';
+
+	config.height = '800';
+
+	config.allowedContent = true;
+};
